1407e190902414d311d65660fc480fb193a8d9ea
George Protacio-Karaszi <georgekaraszi@gmail.com>
2020-10-25 14:50:53 -0700

    Allow color struct to convert between ImVec4 & ImU32

--- ApUI/Types/Color.cpp
+++ ApUI/Types/Color.cpp
@@ -4,23 +4,33 @@
 
 namespace ApUI::Types
 {
-  const Color Color::Red    = { 1.0f, 0.0f, 0.0f };
-  const Color Color::Green  = { 0.0f, 1.0f, 0.0f };
-  const Color Color::Blue   = { 0.0f, 0.0f, 1.0f };
-  const Color Color::White  = { 1.0f, 1.0f, 1.0f };
-  const Color Color::Black  = { 0.0f, 0.0f, 0.0f };
-  const Color Color::Yellow = { 1.0f, 1.0f, 0.0f };
-  const Color Color::Cyan   = { 0.0f, 1.0f, 1.0f };
+  const Color Color::Red        = { 1.0f, 0.0f, 0.0f };
+  const Color Color::Green      = { 0.0f, 1.0f, 0.0f };
+  const Color Color::Blue       = { 0.0f, 0.0f, 1.0f };
+  const Color Color::White      = { 1.0f, 1.0f, 1.0f };
+  const Color Color::Black      = { 0.0f, 0.0f, 0.0f };
+  const Color Color::Yellow     = { 1.0f, 1.0f, 0.0f };
+  const Color Color::Cyan       = { 0.0f, 1.0f, 1.0f };
+  const Color Color::Faction_TR = { 242.0f, 13.0f, 29.0f };
+  const Color Color::Faction_NC = { 49.0f, 57.0f, 247.0f };
+  const Color Color::Faction_VS = { 140.0f, 12.0f, 197.0f };
 
   Color::Color(float p_r, float p_g, float p_b, float p_a)
   : r(p_r), g(p_g), b(p_b), a(p_a)
   {
   }
 
+  Color::Color(ImVec4 &im_color)
+  : Color(im_color.w, im_color.x, im_color.y, im_color.z)
+  {}
+
+  Color::Color(ImColor &im_color)
+  : Color(im_color.Value)
+  {}
+
   bool Color::operator==(const Color &other) const
   {
-    return
-    this->r == other.r &&
+    return this->r == other.r &&
     this->g == other.g &&
     this->b == other.b &&
     this->a == other.a;
@@ -29,6 +39,16 @@ namespace ApUI::Types
   bool Color::operator!=(const Color &other) const
   {
     return !operator==(other);
+  }
+
+  Color::operator ImU32 () const
+  {
+    return ImGui::ColorConvertFloat4ToU32(operator ImVec4());
+  }
+
+  Color::operator ImVec4() const
+  {
+    return ImVec4(r, g, b, a);
   }
 }
 
--- ApUI/Types/Color.hpp
+++ ApUI/Types/Color.hpp
@@ -1,11 +1,14 @@
 #ifndef APHELION_APUI_TYPES_COLOR_HPP
 #define APHELION_APUI_TYPES_COLOR_HPP
 
+#include <ApUI/ImGui/imgui.h>
 namespace ApUI::Types
 {
   struct Color
   {
     Color(float p_r = 1.0f, float p_g = 1.0f, float p_b = 1.0f, float p_a = 1.0f);
+    explicit Color(ImVec4& im_color);
+    explicit Color(ImColor& im_color);
 
     float r, g, b, a;
 
@@ -16,9 +19,14 @@ namespace ApUI::Types
     static const Color Black;
     static const Color Yellow;
     static const Color Cyan;
+    static const Color Faction_TR;
+    static const Color Faction_NC;
+    static const Color Faction_VS;
 
     bool operator==(const Color &other) const;
     bool operator!=(const Color &other) const;
+    inline explicit operator ImU32() const;
+    inline explicit operator ImVec4() const;
   };
 }
 
