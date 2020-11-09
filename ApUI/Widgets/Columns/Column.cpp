0625f3ff5288518e3034047669696bd6f26a22d6
George Protacio-Karaszi <georgekaraszi@gmail.com>
2020-11-08 16:17:50 -0800

    Simplify Columns::Column Widget

--- ApUI/Widgets/Columns/Column.cpp
+++ ApUI/Widgets/Columns/Column.cpp
@@ -5,46 +5,16 @@ namespace ApUI::Widgets::Columns
   Column::Column(bool boarder) : m_boarder(boarder)
   {}
 
-  Column::~Column()
-  {
-    for(const auto& pair : m_columns)
-    {
-      delete pair.first;
-    }
-
-    m_columns.clear();
-  }
-
-  AWidget *Column::GetHeader(int position)
-  {
-    if(position > m_columns.size())
-    {
-      return nullptr;
-    }
-
-    return m_columns[position].first;
-  }
-
-  WidgetContainer *Column::GetColumn(int position)
-  {
-    if(position > m_columns.size())
-    {
-      return nullptr;
-    }
-
-    return &m_columns[position].second;
-  }
-
   void Column::_Draw_Impl()
   {
-    auto col_size = m_columns.size();
+    auto col_size = m_widgets.size();
 
     ImGui::Columns(col_size, m_widget_id.c_str(), m_boarder);
 
     for(int i = 0; i < col_size; i++)
     {
-      m_columns[i].first->Draw();
-      m_columns[i].second.DrawWidgets();
+      m_widgets[i]->Draw();
+
       if((i+1) < col_size)
       {
         ImGui::NextColumn();
--- ApUI/Widgets/Columns/Column.hpp
+++ ApUI/Widgets/Columns/Column.hpp
@@ -1,33 +1,20 @@
 #ifndef APUI_WIDGETS_COLUMNS_COLUMN_HPP
 #define APUI_WIDGETS_COLUMNS_COLUMN_HPP
 
-#include <ApTools/Eventing/Event.hpp>
-#include <ApUI/Widgets/WidgetContainer.hpp>
+#include <ApUI/Widgets/Layout/Group.hpp>
 
 namespace ApUI::Widgets::Columns
 {
-  class Column : public AWidget
+class Column : public Layout::Group
   {
   public:
     explicit Column(bool boarder = false);
-    ~Column();
-
-    AWidget* GetHeader(int position);
-    WidgetContainer* GetColumn(int position);
-
-    template<typename T, typename ...Args>
-    WidgetContainer& CreateColumn(Args&&...args)
-    {
-      m_columns.push_back(std::make_pair(new T(std::forward<Args>(args)...), WidgetContainer()));
-      return m_columns.back().second;
-    }
 
   protected:
     void _Draw_Impl() override;
 
   private:
     bool m_boarder;
-    std::vector<std::pair<AWidget*, WidgetContainer>> m_columns;
   };
 }
 
